cbad288e1b2a0d5432928f4e3c5b52fa2f8bf8f2
Dinesh065 <dc9359349132@gmail.com>
2024-06-24 20:18:02 +0530

    New components added to Header with onClick functionality

--- Projects/news-mag/src/App.jsx
+++ Projects/news-mag/src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react"
 import Navbar from "./components/Navbar"
 import NewsBoard from "./components/NewsBoard"
 
 function App() {
 
+  const [category,setCategory] = useState("general");
 
   return (
     <div>
-      <Navbar/>
-      <NewsBoard/>
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
     </div>
   )
 }
 
 export default App
+
